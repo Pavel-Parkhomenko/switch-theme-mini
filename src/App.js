14aa891429767963f6605079fae27b3ddcf109e1
pawelparhomenko@gmail.com <pawelparhomenko@gmail.com>
2023-03-10 13:23:12 +0300

    init project

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from "react";
+import './App.css';
+import Circle from "./components/Circle";
+import Instructions from "./components/Instructions";
+import Footer from "./components/Footer";
+import Decoration from "./components/Decoration";
+
+
+export default function App() {
+  const [darkMode, setDarkMode] = React.useState(false)
+
+  function toggle() {
+    setDarkMode(prevMode => !prevMode)
+  }
+
+  const darkStyle = {
+    backgroundColor: darkMode ? "#251F28" : "#FFFFFF",
+    color: darkMode ? "#FFFFFF" : "#000000",
+  }
+
+  const darkStyleSlider = {
+    transform: darkMode ? "translateX(100%)" : "translateX(0)",
+    backgroundColor: darkMode ? "#322B36" : "#FFFFFF",
+    boxShadow: darkMode ? "1px 1px 5px #1F1B22" : "2px 2px 5px #CEC4C4",
+  }
+
+  const darkStyleSliderContainer = {
+    backgroundColor: darkMode ? "#000000" : "#E3E3E5",
+  }
+
+  const darkStyleTextDark = {
+    color: darkMode ? "#FFFFFF" : "#74717B"
+  }
+
+  const darkStyleTextLight = {
+    color: darkMode ? "#74717B" : "#000000"
+  }
+  
+  return (  
+    <div>
+       hello
+    </div>
+  );
+}
+
